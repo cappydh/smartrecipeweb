0af23a7d2f2cf7a41f9b2e670f29f48323536813
Ahmet Uraloglu <ahmeturaloglu@msn.com>
2019-11-06 16:19:10 +0300

    spinner

--- smart_recipe/src/components/Spinner.js
+++ smart_recipe/src/components/Spinner.js
@@ -3,7 +3,20 @@ import React from "react";
 const Spinner = props => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">{props.message}</div>
+      {/* <div className="ui big text loader">{props.message}</div> */}
+      <div className="ui big text" style={{ color: "white", fontSize: 30 }}>
+        {props.message}
+      </div>
+      <br />
+      <img
+        src="https://i.gifer.com/p7A.gif"
+        alt="spinner"
+        style={{
+          width: 300,
+          height: 300,
+          borderRadius: 300 / 2
+        }}
+      />
     </div>
   );
 };
