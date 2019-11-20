0f359e6089691e56aad1dc3003712b8c19f31cb5
cappydh <ahmeturaloglu@msn.com>
2019-11-20 10:23:31 +0300

    Updates

---
+++ precache-manifest.d2647373a62dcda5f5cfbe483563183b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2e9f6543422ea1219c056d1816ea7a",
+    "url": "/smartrecipeweb/smart_recipe/index.html"
+  },
+  {
+    "revision": "120d3dc29b5a130bf73f",
+    "url": "/smartrecipeweb/smart_recipe/static/css/main.6766c012.chunk.css"
+  },
+  {
+    "revision": "6ffe77a1c5f6be31e71f",
+    "url": "/smartrecipeweb/smart_recipe/static/js/2.a315c40e.chunk.js"
+  },
+  {
+    "revision": "120d3dc29b5a130bf73f",
+    "url": "/smartrecipeweb/smart_recipe/static/js/main.cc47321b.chunk.js"
+  },
+  {
+    "revision": "b077e6a4660b1c59c950",
+    "url": "/smartrecipeweb/smart_recipe/static/js/runtime-main.f7bff9a0.js"
+  }
+]);
